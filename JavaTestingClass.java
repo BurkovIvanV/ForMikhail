3c96b3b9d7eb3acd8858288346d2cd2fb98ffa36
BurkovIvanV <utka.burkov@yandex.ru>
2019-02-28 10:44:15 +0500

    made void methods

--- JavaTestingClass.java
+++ JavaTestingClass.java
@@ -17,6 +17,12 @@ public class JavaTestingClass {
     private static WebDriver driver;
     private static WebDriverWait wait;
 
+    public static void main(String args[]) {
+        settingDriver();
+        signIn("Ivan.Burkov2043@yandex.ru", "mynalegkesnova333");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Setting and Sign in">
     public static void settingDriver() {
         System.setProperty("webdriver.chrome.driver", "D:\\Иван Вадимович\\СТАЖИРОВКА\\seleniumweb\\driversSel\\chromedriver.exe");
 
@@ -26,53 +32,22 @@ public class JavaTestingClass {
         wait = new WebDriverWait(driver, 10);
     }
 
-    public static void main(String args[]) {
-        settingDriver();
-        signIn("Ivan.Burkov2043@yandex.ru", "mynalegkesnova333");
 
-
-
-        //swithchOverLanguage();
-        // deletingMessages("utka.burkov@yandex.ru");
-        // sendMessage();
-        // deletingMessages("utka.burkov@yandex.ru");
-
-        // driver.close();
-       // System.out.println(isEmailCorrect("utka.burkov@yandex.ru"));
-    }
-
-
-    public static String signIn(String userEmail, String userPassword) {
+    public static void signIn(String userEmail, String userPassword) {
         driver.get("https://passport.yandex.ru");
         driver.findElement(By.id("passp-field-login")).sendKeys(userEmail);
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         driver.findElement(By.id("passp-field-passwd")).sendKeys(userPassword);
         driver.findElement(By.xpath("//button[@type='submit']")).click();
-
-        String result = driver.findElement(By.xpath("//div[@class='personal__security-level']")).getText();
-
         driver.findElement(By.xpath("//span[text()='van.Burkov2043']")).click();
         driver.findElement(By.xpath("/html/body/div[2]/div/ul/ul/li[2]/a")).click();
-
-
-        return result;
     }
+    // </editor-fold>
 
-    public static String swithchOverLanguage() {
-
-        clickSettingButton();
-        openLanguageList();
-        By languageXpath = By.xpath("//div[@class='b-mail-dropdown__box__content']/div[1]");
-        String newLanguage = driver.findElement(languageXpath).getText().trim();
-        driver.findElement(languageXpath).click();
-        //wait.until(ExpectedConditions.visibilityOfElementLocated(setting));
-        try {
-            Thread.sleep(4000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        driver.findElement(By.xpath("//a[@class='b-setup-title__link']")).click();
-        return newLanguage;
+    // <editor-fold defaultstate="collapsed" desc="Setting Language">
+    public static void switchOverLanguage() {
+        driver.findElement(By.xpath("//div[@class='b-mail-dropdown__box__content']/div[1]")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='b-setup-title__link']")));
     }
 
     public static void clickSettingButton() {
@@ -84,178 +59,164 @@ public class JavaTestingClass {
     public static void openLanguageList() {
         driver.findElement(By.xpath("//span[contains(@class, 'Settings-Lang_arrow')]")).click();
     }
+// </editor-fold>
 
-    public static ArrayList<WebElement> getMessagesOnPage() {
-        ArrayList<WebElement> messagesOnPage = (ArrayList<WebElement>) driver.findElements(By.xpath(
-                "//div[@class='ns-view-container-desc mail-MessagesList js-messages-list']/div//span[@class='mail-MessageSnippet-FromText']"));
-        return messagesOnPage;
-    }
-
-
-    public static ArrayList<WebElement> deletingMessages(String email) {
-        ArrayList<WebElement> messagesOnPage = getMessagesOnPage();
-        ArrayList<WebElement> deletedMessages;
-        deletedMessages = (ArrayList<WebElement>) driver.findElements(By.xpath("//span[@title='" + email + "']"));
-        for (WebElement message : messagesOnPage) {
+    // <editor-fold defaultstate="collapsed" desc="Deleting Messages">
+    public static void tickMessages(String email) {
+        int n = driver.findElements(By.xpath("//div[@class='ns-view-container-desc mail-MessagesList js-messages-list']/div")).size();
+        for (int i = 1; i <= n; i++) {
+            WebElement message = driver.findElement(By.xpath("//div[@class='ns-view-container-desc mail-MessagesList js-messages-list']/div["
+                    + i + "]//span[@class=\"mail-MessageSnippet-FromText\"]"));
             if (message.getAttribute("title").equals(email)) {
-                int numberOfMessage = messagesOnPage.indexOf(message) + 1;
-                driver.findElement(By.xpath("//div[@class='ns-view-container-desc mail-MessagesList js-messages-list']" +
-                        "/div[" + numberOfMessage + "]//span[@class='_nb-checkbox-flag _nb-checkbox-normal-flag']")).click();
-                //break;
+                driver.findElement(By.xpath("//div[contains(@class,'js-messages-list')]" +
+                        "/div[" + i + "]//span[@class='_nb-checkbox-flag _nb-checkbox-normal-flag']")).click();
             }
         }
-        return deletedMessages;
     }
 
     public static void clickDeleteMessageButton() {
         driver.findElement(By.xpath("//span[contains(@class,'js-toolbar-item-title-delete')]")).click();
     }
+// </editor-fold>
 
-
-    public static void sendingMessageTrueEmail() {
-        clickComposeButton();
-        String email = "utka.burkov@yandex.ru";
-        By buttonWriteMessage = By.xpath("//span[@class='mail-ComposeButton-Text']");
-        driver.findElement(buttonWriteMessage).click();
+    // <editor-fold defaultstate="collapsed" desc="Sendung Message">
+    public static void clickComposeButton() {
+        By buttonSend = By.xpath("//span[@class='mail-ComposeButton-Text']");
+        driver.findElement(buttonSend).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='mail-ui-Link mail-ui-ComplexLink']")));
-        By receiver = By.xpath("//div[@class = 'js-compose-field mail-Bubbles']");
-        driver.findElement(receiver).sendKeys(email);
-        By title = By.xpath("//input[contains(@class,'js-compose-field js-editor-tabfocus')]");
-        driver.findElement(title).sendKeys("Тестирование");
-        By textOfMessage = By.xpath("//div[contains(@class,'cke_show_borders')]");
-        driver.findElement(textOfMessage).sendKeys("testing test");
-        //driver.switchTo().alert().accept();
-       // clickSendMessageButton();
     }
 
-    public static void sendingMessageFalseEmail() {
-        clickComposeButton();
-        String email = "utka.burkovyandex.ru";
-        By buttonWriteMessage = By.xpath("//span[@class='mail-ComposeButton-Text']");
-        driver.findElement(buttonWriteMessage).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='mail-ui-Link mail-ui-ComplexLink']")));
-        By receiver = By.xpath("//div[@class = 'js-compose-field mail-Bubbles']");
+    public static void writingEmail(String email) {
+        //By receiver = By.xpath("//div[@class = 'js-compose-field mail-Bubbles']");
+        driver.findElement(By.xpath("//div[@class = 'js-compose-field mail-Bubbles']")).sendKeys(email);
+    }
 
-        driver.findElement(receiver).sendKeys(email);
-        By title = By.xpath("//input[contains(@class,'js-compose-field js-editor-tabfocus')]");
-        driver.findElement(title).sendKeys("Тестирование");
+    public static void writingTitle(String title) {
+        driver.findElement(By.xpath("//input[contains(@class,'js-compose-field js-editor-tabfocus')]"))
+                .sendKeys(title);
+    }
+
+    public static void writingTextMessage(String text) {
         By textOfMessage = By.xpath("//div[contains(@class,'cke_show_borders')]");
         driver.findElement(textOfMessage).sendKeys("testing test");
-        //clickSendMessageButton();
-        //driver.switchTo().alert().accept();
     }
+
 
     public static void clickSendMessageButton() {
         By buttonSend = By.xpath("//div[contains(@class,'Footer-Main')]//button[contains(@class, 'ui-button-text-only')]");
         driver.findElement(buttonSend).click();
     }
-    public static void clickComposeButton(){
-        By buttonSend = By.xpath("//span[@class='mail-ComposeButton-Text']");
-        driver.findElement(buttonSend).click();
-    }
+
+
+    // </editor-fold>
 
     public static boolean isElementExists(By xpathElement) {
         return driver.findElements(xpathElement).size() > 0;
     }
 
-    public static boolean isEmailCorrect (String email) {
+    public static boolean isEmailCorrect(String email) {
         String emailPattern = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
         Pattern pattern = Pattern.compile(emailPattern);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
 
-    @Test
-    public void sigInTest() {
-        settingDriver();
-        String res = signIn("Ivan.Burkov2043@yandex.ru", "mynalegkesnova333");
-        if (res.equals("Account is well protected") || res.equals("Аккаунт надёжно защищён"))
-            Assert.assertTrue(true);
-        else Assert.assertTrue(false);
+    public static String getCurrentLanguage() {
+        return driver.findElement(By.xpath
+                ("//span[@class='b-selink__inner']/span[@class='b-selink__link mail-Settings-Lang']")).getText();
     }
 
+    public static ArrayList<WebElement> getTickedMessage() {
+        return (ArrayList<WebElement>) driver.findElements(
+                By.xpath("//div[@class='ns-view-container-desc mail-MessagesList js-messages-list']/div[contains(@class, 'is-checked')]"));
+    }
+
+    public static ArrayList<WebElement> getAllMessagesOnPage() {
+        return (ArrayList<WebElement>) driver.findElements(
+                By.xpath("//div[@class='ns-view-container-desc mail-MessagesList js-messages-list']/div"));
+    }
+
+    //@Step
+    public static void checkSendingMessage(String email) {
+        if (isElementExists(By.xpath("//div[contains(@class,'mail-Compose-Field-Footnote_error')]")) && isEmailCorrect(email))
+            Assert.assertTrue(true);
+        else if (isEmailCorrect(email) == false)
+            Assert.fail("Email is invalid");
+        else Assert.assertTrue(false);
+    }
 
     @Test
     public static void switchOverLanguageTest() {
         settingDriver();
         signIn("Ivan.Burkov2043@yandex.ru", "mynalegkesnova333");
-
-        String trueLanguage = swithchOverLanguage();
-
-        String currentLanguage =
-                driver.findElement(By.xpath("//a[@class='mail-ui-Link mail-ui-ComplexLink']")).getAttribute("title").trim();
-        Assert.assertEquals(trueLanguage, currentLanguage);
-    }
-
-    @Test
-    public void deletingMessagesTestWithoutClick() {
-        settingDriver();
-        signIn("Ivan.Burkov2043@yandex.ru", "mynalegkesnova333");
-
-        ArrayList<WebElement> deletedMessages;
-        ArrayList<WebElement> messagesOnPage;
-        deletedMessages = deletingMessages("utka.burkov@yandex.ru");
-        messagesOnPage = getMessagesOnPage();
-        for (WebElement deletedMessage : deletedMessages) {
-            if (messagesOnPage.contains(deletedMessage))
-                Assert.assertTrue(false);
-        }
-        Assert.assertTrue(true);
-    }
-    @Test
-    public static void deletingMessagesTestWithClick() {
-        settingDriver();
-        signIn("Ivan.Burkov2043@yandex.ru", "mynalegkesnova333");
-
-        ArrayList<WebElement> deletedMessages;
-        ArrayList<WebElement> messagesOnPage;
-        deletedMessages = deletingMessages("utka.burkov@yandex.ru");
-        clickDeleteMessageButton();
-        messagesOnPage = getMessagesOnPage();
-        for (WebElement deletedMessage : deletedMessages) {
-            if (messagesOnPage.contains(deletedMessage))
-                Assert.assertTrue(false);
-        }
-        Assert.assertTrue(true);
+        clickSettingButton();
+        openLanguageList();
+        String languageBefore = getCurrentLanguage();
+        switchOverLanguage();
+        Assert.assertNotEquals(getCurrentLanguage(), languageBefore);
     }
 
     @Test
     public static void sendingMessageTestWithTrueEmail() {
         settingDriver();
         signIn("Ivan.Burkov2043@yandex.ru", "mynalegkesnova333");
-
-        sendingMessageTrueEmail();
-        String currentEmail = driver.findElement
-                (By.xpath("//div[@class='js-compose-field mail-Bubbles mail-Bubbles-Sms']/span")).getAttribute("data-yabble-email");
+        clickComposeButton();
+        String email = "ivan.burkov2043@yandex.ru";
+        writingEmail(email);
+        writingTitle("Testing");
         clickSendMessageButton();
-        By titleInfoXpath = By.xpath("//div[@class='mail-Done-Title js-title-info']");
-        boolean isEmailCorrect = isEmailCorrect(currentEmail);
-        if (isElementExists(titleInfoXpath) && isEmailCorrect) {
-            Assert.assertTrue(true);
-        } else if (isEmailCorrect == false && isElementExists(By.xpath("//div[contains(@class,'mail-Compose-Field-Footnote_error')]"))) {
-            Assert.assertTrue(true);
-        } else Assert.assertTrue(false);
+        checkSendingMessage(email);
     }
 
     @Test
     public static void sendingMessageTestWithWrongEmail() {
+
         settingDriver();
         signIn("Ivan.Burkov2043@yandex.ru", "mynalegkesnova333");
-
-        sendingMessageFalseEmail();
-
-        String currentEmail = driver.findElement
-                (By.xpath("//div[@class='js-compose-field mail-Bubbles mail-Bubbles-Sms']/span")).getAttribute("data-yabble-email");
+        clickComposeButton();
+        String email = "ivan.burkov2043@yandexru";
+        writingEmail(email);
+        writingTitle("Testing");
         clickSendMessageButton();
-        By titleInfoXpath = By.xpath("//div[@class='mail-Done-Title js-title-info']");
-        boolean isEmailCorrect = isEmailCorrect(currentEmail);
-
-        if (isElementExists(titleInfoXpath)==true && isEmailCorrect==true) {
-            Assert.assertTrue(true);
-        } else if (isEmailCorrect == false && isElementExists(By.xpath("//div[contains(@class,'mail-Compose-Field-Footnote_error')]"))) {
-            Assert.assertTrue(true);
-        } else Assert.assertTrue(false);
+        checkSendingMessage(email);
     }
 
+    @Test
+    public static void deletingMessagesTestWithClick() {
+        settingDriver();
+        signIn("Ivan.Burkov2043@yandex.ru", "mynalegkesnova333");
+        tickMessages("utka.burkov@yandex.ru");
+        ArrayList<WebElement> deletedMessages = getTickedMessage();
+        clickDeleteMessageButton();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='mail-ui-Link mail-ui-ComplexLink']")));
+        ArrayList<WebElement> allMesages = getAllMessagesOnPage();
+        for (WebElement deletedMessage : deletedMessages) {
+            if (allMesages.contains(deletedMessage))
+                Assert.assertTrue(false);
+        }
+    }
 
+    @Test
+    public void sigInTest() {
+        settingDriver();
+        signIn("Ivan.Burkov2043@yandex.ru", "mynalegkesnova333");
+        Assert.assertTrue(isElementExists(By.xpath("//a[@class='mail-ui-Link mail-ui-ComplexLink']")));
+
+    }
+
+    @Test
+    public void deletingMessagesTestWithoutClick() {
+        settingDriver();
+        signIn("Ivan.Burkov2043@yandex.ru", "mynalegkesnova333");
+        tickMessages("utka.burkov@yandex.ru");
+        ArrayList<WebElement> deletedMessages = getTickedMessage();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='mail-ui-Link mail-ui-ComplexLink']")));
+        ArrayList<WebElement> allMesages = getAllMessagesOnPage();
+        for (WebElement deletedMessage : deletedMessages) {
+            if (allMesages.contains(deletedMessage)) {
+                //Assert.assertTrue(false);
+                Assert.fail("Delete message button was not pressed");
+            }
+        }
+    }
 }
